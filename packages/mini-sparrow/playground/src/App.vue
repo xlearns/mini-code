d6cfb36064fd3c1c2d6b80e106c6ac76b24ecd30
root <root@DESKTOP-HPSBQ9J.localdomain>
2022-10-08 23:30:27 +0800

    feat: add playground for mini-sparrow

---
+++ packages/mini-sparrow/playground/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { render } from "../../packages";
+onMounted(() => {
+  render();
+});
+</script>
+<template>
+  <canvas class="canvas"></canvas>
+</template>
+<style scoped>
+.canvas {
+  height: 100%;
+}
+</style>
